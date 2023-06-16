f00aca5eec9b7e9b11209967d10ddc1164699bd0
Debiisittner <debiisittner94@gmail.com>
2023-06-15 23:50:15 -0300

    segundo commit

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,14 @@ let menu_bar = document.getElementById("menu-bar")
 menu_bar.addEventListener("click",function (){
     menu.classList.toggle("menu-toggle")
 })
+hambre = true
+
+hambre = prompt("tenes hambre");
+
+if (hambre == si){
+    comida = prompt("decime que queres comer");
+    alert("ahi te cocino"+comida);
+} else{
+    comida= alert("que bueno que ya comiste");
+}
 
